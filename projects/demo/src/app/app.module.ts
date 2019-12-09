5c0be6c84fcdb4e8b0074a0df4f2cbd43aaa6f3f
chazing <33194200+Chazing@users.noreply.github.com>
2019-12-09 11:34:22 +0200

    Add files via upload

---
+++ projects/demo/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { SharedModule } from '@aa/shared';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
